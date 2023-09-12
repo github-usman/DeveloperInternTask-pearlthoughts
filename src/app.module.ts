c5e65b29240af1c2498f85e3586177a8490a518f
github-usman <usmantoyou@gmail.com>
2023-09-12 13:53:30 +0530

    hide credentials using .env file and monogoose conneted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.contorller';
-import {MongooseModule} from "@nestjs/mongoose";
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports:[MongooseModule.forRoot(" mongodb://127.0.0.1:27017/task-one")],
-  controllers: [UsersController]
+ 
+  imports:[
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory:(ConfigService:ConfigService)=>{
+          const port = ConfigService.get("DATABASE_PORT");
+          const ip = ConfigService.get("DATABASE_IP");
+          const db = ConfigService.get("DATABASE_NAME");
+          const uri = `mongodb://${ip}:${port}/${db}`
+          return {
+            uri,
+          };
+      },
+      inject:[ConfigService],
+    }),
   
+  ],
+
+
+  controllers:[UsersController
+  ],
 })
 export class AppModule {}
