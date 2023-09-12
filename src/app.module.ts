dcb3dc6499f81da1fd85e083e5b3ed0dd76684b6
github-usman <usmantoyou@gmail.com>
2023-09-12 10:37:29 +0530

    mongoose connectivity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.contorller';
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
- 
+  imports:[MongooseModule.forRoot(" mongodb://127.0.0.1:27017/task-one")],
   controllers: [UsersController]
   
 })
